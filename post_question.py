bbc192dbc2186e0fff59f10ed08b5870ae8c880e
wilbertaristo <45594146+wilbertaristo@users.noreply.github.com>
2022-07-30 02:03:31 +0800

    Recursive question randomizer & deletion

--- post_question.py
+++ post_question.py
@@ -13,16 +13,29 @@ done_ws = spreadsheet.worksheet("Done")
 
 # ---------------------- Super Basic Implementation ----------------------
 
-random_question = pending_ws.row_values(random.randint(2, len(pending_ws.get_values()))) # Get random question
-[question, image_url] = [i.strip() for i in random_question] # Destructuring + strip check
+def get_random_qn(): 
+  random_index = random.randint(2, len(pending_ws.get_values()))
+  random_question = pending_ws.row_values(random_index)
 
-if question not in done_ws.col_values(1): # Check that the question has never been asked before in Done worksheet
-  
+  [question, image_url] = [i.strip() for i in random_question] # Destructuring + strip check
+
+  if question in done_ws.col_values(1): # If question has been asked before in `Done` worksheet
+    pending_ws.delete_rows(random_index) # Delete from `Pending` worksheet
+    get_random_qn() # Find a new question
+  else:
+    return [question, image_url, random_index] # Return valid random question
+
+try:
+  [question, image_url, random_index] = get_random_qn()
+
   # --- Execute peepo bot command ---
 
   # requests.post("PEEPO_API_URL", json = { "question": question, "url": image_url })
-  
+
   # ---------------------------------
 
-  pending_ws.delete_rows(2) # Delete posted question from Pending worksheet
-  done_ws.insert_row(random_question, len(done_ws.get_values()) + 1) # Add posted question to Done worksheet
+  pending_ws.delete_rows(random_index) # Delete posted question from `Pending` worksheet
+  done_ws.insert_row([question, image_url], len(done_ws.get_values()) + 1) # Add posted question to `Done` worksheet
+
+except ValueError:
+  print("Out of Questions!")

ab5f5a62e73b4e20f89c62089dad062a9f0e2666
Wilbert Aristo <wilbert.aristo@hotmail.com>
2022-07-29 11:24:06 +0800

    Randomize questions

--- post_question.py
+++ post_question.py
@@ -1,5 +1,6 @@
 import gspread
-import subprocess
+import requests
+import random
 
 # API Documentations => https://docs.gspread.org/en/latest/api/models/worksheet.html
 
@@ -12,16 +13,16 @@ done_ws = spreadsheet.worksheet("Done")
 
 # ---------------------- Super Basic Implementation ----------------------
 
-oldest_question = pending_ws.row_values(2) # Get oldest question which is second row of pending worksheet
-[question, image_url] = [i.strip() for i in oldest_question] # Destructuring + strip check
+random_question = pending_ws.row_values(random.randint(2, len(pending_ws.get_values()))) # Get random question
+[question, image_url] = [i.strip() for i in random_question] # Destructuring + strip check
 
 if question not in done_ws.col_values(1): # Check that the question has never been asked before in Done worksheet
   
   # --- Execute peepo bot command ---
 
-  # subprocess.run(f"curl peepo_post question='{question}' url='{image_url}'") 
+  # requests.post("PEEPO_API_URL", json = { "question": question, "url": image_url })
   
   # ---------------------------------
 
   pending_ws.delete_rows(2) # Delete posted question from Pending worksheet
-  done_ws.insert_row(oldest_question, len(done_ws.get_values()) + 1) # Add posted question to Done worksheet
+  done_ws.insert_row(random_question, len(done_ws.get_values()) + 1) # Add posted question to Done worksheet

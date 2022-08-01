aa565f665acbd5d563904bd6d471529eb2e080f9
Wilbert Aristo <wilbert.aristo@hotmail.com>
2022-08-01 18:44:01 +0800

    Integrate with Slack API

--- post_question.py
+++ post_question.py
@@ -1,10 +1,19 @@
 import gspread
 import requests
 import random
+import os
+from dotenv import load_dotenv
 
 # API Documentations => https://docs.gspread.org/en/latest/api/models/worksheet.html
 
-#----------------------   Setups   ----------------------
+# ----------------------   Setups   ----------------------
+load_dotenv()
+
+WEBHOOK_API = os.getenv("WEBHOOK_API")
+API_URL = os.getenv("API_URL")
+AUTH_TOKEN = os.getenv('AUTH_TOKEN')
+CHANNEL_ID = os.getenv('CHANNEL_ID')
+
 service_account = gspread.service_account(filename="watercooler-trivia-bot.json")
 spreadsheet = service_account.open("Ascenda Watercooler Trivias")
 
@@ -13,7 +22,7 @@ done_ws = spreadsheet.worksheet("Done")
 
 # ---------------------- Super Basic Implementation ----------------------
 
-def get_random_qn(): 
+def get_random_qn():
   random_index = random.randint(2, len(pending_ws.get_values()))
   random_question = pending_ws.row_values(random_index)
 
@@ -28,11 +37,13 @@ def get_random_qn():
 try:
   [question, image_url, random_index] = get_random_qn()
 
-  # --- Execute peepo bot command ---
-
-  # requests.post("PEEPO_API_URL", json = { "question": question, "url": image_url })
-
-  # ---------------------------------
+  post_request = requests.post(API_URL, headers={
+      "Content-type": "application/json",
+      "Authorization": f"Bearer {AUTH_TOKEN}",
+  }, json={
+      "channel": CHANNEL_ID,
+      "attachments": [{"text": question, "image_url": image_url}]
+  })
 
   pending_ws.delete_rows(random_index) # Delete posted question from `Pending` worksheet
   done_ws.insert_row([question, image_url], len(done_ws.get_values()) + 1) # Add posted question to `Done` worksheet
